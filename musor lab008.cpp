55d374cd941b341472e59956acd98b8f53c0d0b3
ffa66-cloud <f.fa.66@mail.ru>
2025-10-21 20:26:10 +0300

    Create musor lab008.cpp

---
+++ musor lab008.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+int add(T a[], int s, T e) {
+    a[s] = e;
+    return s + 1;
+}
+int main()
+{
+    int s =3;
+    int a[s]={566548,4968412,8767239};
+    for(int i=0; i<s; i++)
+    cout<<a[i]<<"\n";
+    cout<<"\nразмер "<<s<<"\n"<<"new\n"<<endl;
+    s= add(a, s, 7);
+    for(int i=0; i<s; i++)
+    cout<<a[i]<<endl;
+    cout<<"\nразмер "<<s;
+};
